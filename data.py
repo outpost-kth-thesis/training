d2248631e17e7f215085be4c3d9df77e32966d66
ohshitnotgood <praanto@icloud.com>
2025-05-15 17:12:25 +0200

    added tokenization in __getitem__ instead of _walk_directory_

--- data.py
+++ data.py
@@ -12,22 +12,23 @@ class LanguageDataset(Dataset):
         self.t = LanguageTokenizer()
         self._walk_root_directory()
 
+
     def __getitem__(self, index):
-        return self.all_content[index]
+        minified_filepath = self.all_files[index]
+        original_filepath = minified_filepath.replace("_terser.min.js", ".js") if "_terser" in minified_filepath else minified_filepath.replace("_google.min.js", ".js")
+        minified_file_content = open(file=minified_filepath).read()
+        original_file_content = open(file=original_filepath).read()
+        json = {
+            "minified_file_content":minified_file_content,
+            "original_file_content":original_file_content
+        }
+        return self.t.tokenize(json)
 
     def _walk_root_directory(self):
         for root, _, files in os.walk(dataset_dir):
             for file in files:
                 if ".min.js" in file:
-                    original_filepath = file.replace("_terser.min.js", ".js") if "_terser" in file else file.replace("_google.min.js", ".js")
-                    minified_file_content = open(file=os.path.join(root, file)).read()
-                    original_file_content = open(file=os.path.join(root, original_filepath)).read()
-                    json = {
-                        "minified_file_content":minified_file_content,
-                        "original_file_content":original_file_content
-                    }
-                    tokenized = self.t.tokenize(json)
-                    self.all_content.append(tokenized)
+                    self.all_files.append(os.path.join(root, file))
 
 if __name__ == "__main__":
     dt = LanguageDataset()
