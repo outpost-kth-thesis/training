e6c94705383d9ee76661a6f8b93fcf40b4b2001f
ohshitnotgood <praanto@icloud.com>
2025-05-18 17:05:03 +0200

    removed training function

--- train.py
+++ train.py
@@ -9,54 +9,49 @@ from peft import LoraConfig, TaskType, PeftModel, PeftConfig, get_peft_model
 from safetensors.torch import save_model
 
 
-def train_loop():
+dataset = LanguageDataset()
 
-    dataset = LanguageDataset()
+quantization_configs = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_compute_dtype=torch.bfloat16,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_use_double_quant=True,
+)
 
-    quantization_configs = BitsAndBytesConfig(
-        load_in_4bit=True,
-        bnb_4bit_compute_dtype=torch.bfloat16,
-        bnb_4bit_quant_type="nf4",
-        bnb_4bit_use_double_quant=True,
-    )
+model = AutoModelForCausalLM.from_pretrained(model_name, device_map="auto", quantization_config=quantization_configs)
+t = LanguageTokenizer().tokenizer
+model.resize_token_embeddings(len(t), mean_resizing=True)
 
-    model = AutoModelForCausalLM.from_pretrained(model_name, device_map="auto", quantization_config=quantization_configs)
-    t = LanguageTokenizer().tokenizer
-    model.resize_token_embeddings(len(t), mean_resizing=True)
+training_args = TrainingArguments(
+    output_dir="./training_output",
+    per_device_train_batch_size=batch_size,
+    num_train_epochs=epochs,
+    save_steps=10,
+    save_strategy="steps",
+    save_safetensors=False,
+    logging_steps=10,
+)
 
-    training_args = TrainingArguments(
-        output_dir="./training_output",
-        per_device_train_batch_size=batch_size,
-        num_train_epochs=epochs,
-        save_steps=10,
-        save_strategy="steps",
-        save_safetensors=False,
-        logging_steps=10,
-    )
+lora_config = LoraConfig(
+    task_type=TaskType.CAUSAL_LM,
+    inference_mode=False, 
+    r=8,
+    lora_alpha=32,
+    lora_dropout=0.1
+)
 
-    lora_config = LoraConfig(
-        task_type=TaskType.CAUSAL_LM,
-        inference_mode=False, 
-        r=8,
-        lora_alpha=32,
-        lora_dropout=0.1
-    )
-
-    peft_model = get_peft_model(model=model, peft_config=lora_config)
+peft_model = get_peft_model(model=model, peft_config=lora_config)
 
 
-    data_collator = DataCollatorForLanguageModeling(tokenizer=t, mlm=False)
-    trainer = Trainer(
-        model=peft_model,
-        processing_class=t,
-        args=training_args,
-        train_dataset=dataset,
-        data_collator=data_collator
-    )
+data_collator = DataCollatorForLanguageModeling(tokenizer=t, mlm=False)
+trainer = Trainer(
+    model=peft_model,
+    processing_class=t,
+    args=training_args,
+    train_dataset=dataset,
+    data_collator=data_collator
+)
 
 
-    trainer.train()
+trainer.train()
 
-
-if __name__ == "__main__":
-    train_loop()
